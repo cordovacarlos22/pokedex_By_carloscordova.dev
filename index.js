7cd0dce98279d201a102036801452c7f38fda885
carloscordova.dev <cordovamanuel22@icloud.com>
2024-05-16 07:13:26 -0700

    feat(JS): :sparkles: SEARCH POKEMONS NOW WORKINKG.

--- index.js
+++ index.js
@@ -1,66 +1,59 @@
-class Pokemon{
+class Pokemon {
   constructor(name) {
     this.name = name;
   }
+
   async getData() {
     let count = 0;
     let data;
-    let pokemons = []
+    let pokemons = [];
     for (let i = 0; i <= 150; i++) {
       count++;
-      let responce = await fetch(`https://pokeapi.co/api/v2/pokemon/${count}`);
-      data = await responce.json();
-      
+      let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${count}`);
+      data = await response.json();
       pokemons.push(data);
-      
-      
     }
-    console.log(data);
-    
-    
-    return pokemons
+    return pokemons;
   }
 }
 
 addEventListener('load', async () => {
   const pokemon = new Pokemon();
-  let result  = await pokemon.getData();
-  let print =  result.map((item) => {
-    return (
-      `
+  let result = await pokemon.getData();
+  let print = result.map((item) => {
+    return `
       <div key=${item.id} class="pokemon-card">
-      <h1>${item.order}</h1>
-      <h2>${item.name}</h2>
-      <img src="${item.sprites.front_default}" alt="${item.name}">
-      <p>${item.types[0].type.name}</p>
-      <p>${item.height}</p>
-      <p>${item.weight}</p>
-      
+        <h1>${item.order}</h1>
+        <h2>${item.name}</h2>
+        <img src="${item.sprites.front_default}" alt="${item.name}">
+        <p>${item.types[0].type.name}</p>
+        <p>${item.height}</p>
+        <p>${item.weight}</p>
       </div>
-      `
-    )
-  }).join('')
+    `;
+  }).join('');
 
-  let pokemon_container = document.querySelector('.pokemon-container')
+  let pokemon_container = document.querySelector('.pokemon-container');
   pokemon_container.innerHTML = print;
 
+  // Attach each Pokémon's HTML element to the Pokémon object
+  result.forEach(pokemon => {
+    pokemon.element = pokemon_container.querySelector(`[key='${pokemon.id}']`);
+  });
 
-  
+  let searchInput = document.querySelector('.pokemon-search');
 
+  searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase();
+
+    result.forEach(pokemon => {
+      const isVisible =
+        pokemon.name.toLowerCase().includes(value) ||
+        pokemon.types[0].type.name.toLowerCase().includes(value) ||
+        pokemon.height.toString().includes(value) ||
+        pokemon.weight.toString().includes(value);
+
+      pokemon.element.classList.toggle("hide", !isVisible);
+    });
+  });
 });
-
-
-
-
-addEventListener('load', (data) => {
-  
-
-
-}
-)
-  
-
-
-
-
-
